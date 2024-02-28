4a7eaa7bc753693f99f916c537cbec056b1e7dd8
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-02-28 15:36:01 +0000

    movie added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,12 @@
 class MoviesController < ApplicationController
   def new
+    @the_movie = Movie.new
+
     render template: "movies/new"
   end
 
   def edit
-    @the_movie=Movie.where(id: params.fetch(:id))[0]
+    @the_movie = Movie.where(id: params.fetch(:id))[0]
     render template:"movies/edit"
   end
 
@@ -18,32 +20,32 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
-    @the_movie = matching_movies.at(0)
+    @the_movie = matching_movies[0]
 
     render({ :template => "movies/show" })
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
     else
-      redirect_to("/movies/new", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movies/new"
     end
   end
 
   def update
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -58,8 +60,8 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.destroy
 
